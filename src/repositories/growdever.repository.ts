5a46fefbd518e5f499eeb8c6ee9c0fb3f777ebec
Edson Martins <edsonmartins3110@gmail.com>
2022-11-12 17:48:33 -0300

    refatora para utilizar o pghelper centraliza os metodos da controller em um unico

--- src/repositories/growdever.repository.ts
+++ src/repositories/growdever.repository.ts
@@ -1,5 +1,4 @@
 import { Growdever } from "../models/growdever";
-import db from "../db";
 import { pgHelper } from "../database/pg-helper";
 
 export class GrowdeverRepository {
@@ -19,7 +18,7 @@ export class GrowdeverRepository {
   }
 
   async createGrowdever(growdever: Growdever): Promise<void> {
-    await db.query(
+    await pgHelper.client.query(
       "INSERT INTO growdevers(id, name, birth, cpf, status, skills) VALUES ($1, $2, $3, $4, $5, $6)",
       [
         growdever.id,
@@ -33,24 +32,25 @@ export class GrowdeverRepository {
   }
 
   async getGrowdeverByUid(id: string): Promise<Growdever | undefined> {
-    const result = await db.query("SELECT * FROM growdevers WHERE id = $1", [
-      id,
-    ]);
+    const result: Array<any> = await pgHelper.client.query(
+      "SELECT * FROM growdevers WHERE id = $1",
+      [id]
+    );
 
-    if (result.rowCount === 0) return undefined;
+    if (result.length === 0) return undefined;
 
     return Growdever.create(
-      result.rows[0].id,
-      result.rows[0].name,
-      result.rows[0].cpf,
-      result.rows[0].birth,
-      result.rows[0].status,
-      result.rows[0].skills ? (result.rows[0].skills as string).split(",") : []
+      result[0].id,
+      result[0].name,
+      result[0].cpf,
+      result[0].birth,
+      result[0].status,
+      result[0].skills ? (result[0].skills as string).split(",") : []
     );
   }
 
   async updateGrowdever(growdever: Growdever): Promise<void> {
-    await db.query(
+    await pgHelper.client.query(
       "UPDATE growdevers SET name = $1, birth = $2, status = $3, skills = $4 WHERE id = $5",
       [
         growdever.name,
@@ -63,7 +63,12 @@ export class GrowdeverRepository {
   }
 
   async deleteGrowdever(id: string): Promise<boolean> {
-    const result = await db.query("DELETE FROM growdevers WHERE id = $1", [id]);
-    return result.rowCount !== 0;
+    const [result, rowCount]: Array<any> = await pgHelper.client.query(
+      "DELETE FROM growdevers WHERE id = $1",
+      [id]
+    );
+    console.log(result);
+    console.log(rowCount);
+    return rowCount !== 0;
   }
 }
